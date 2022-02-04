1c97391b636f6a752a03589995ea7967c8273f54
Eric Souza <eric.bh18souza@gmail.com>
2022-02-03 21:04:18 -0300

    Context updating feedback logic developed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Header from './components/Header';
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
-import AboutIconLink from './components/AboutIconLink';
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
-import AboutPage from './pages/AboutPage';
-
-import { FeedbackProvider } from './context/FeedbackContext';
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
   return (
     <FeedbackProvider>
       <Router>
         <Header />
-
         <div className="container">
           <Routes>
             <Route
